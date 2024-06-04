5ddb3482c83cc0130f3de81f6cfeb88d0b31f21e
julienhouyet <julien.houyet@gmail.com>
2024-06-04 16:36:02 +0200

    feat: add Zombie.hpp

--- ex00/include/Zombie.hpp
+++ ex00/include/Zombie.hpp
@@ -6,7 +6,24 @@
 /*   By: jhouyet <jhouyet@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/04 16:05:05 by jhouyet           #+#    #+#             */
-/*   Updated: 2024/06/04 16:08:14 by jhouyet          ###   ########.fr       */
+/*   Updated: 2024/06/04 16:35:40 by jhouyet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#pragma once
+#ifndef __ZOMBIE_H__
+#define __ZOMBIE_H__
+
+class Zombie
+{
+	public:
+		Zombie();
+		~Zombie();
+
+		void announce(void);
+		
+	private:
+		std::string _name;
+};
+
+#endif
