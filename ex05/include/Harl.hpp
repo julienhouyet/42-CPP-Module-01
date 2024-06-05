d34bdd7bf25628ea1b4d7f2da6b9ef55331fef2b
Julien Houyet <julien.houyet@gmail.com>
2024-06-05 17:29:03 +0200

    feat: create Harl class

--- ex05/include/Harl.hpp
+++ ex05/include/Harl.hpp
@@ -6,7 +6,31 @@
 /*   By: jhouyet <jhouyet@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/05 17:12:48 by jhouyet           #+#    #+#             */
-/*   Updated: 2024/06/05 17:12:48 by jhouyet          ###   ########.fr       */
+/*   Updated: 2024/06/05 17:28:20 by jhouyet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#pragma once
+#ifndef __HARL_H_
+#define __HARL_H_
+
+#include <iostream>
+
+class Harl
+{
+	public:
+		Harl();
+		~Harl();
+
+		void complain(std::string level);
+		
+	private:
+		std::string _level;
+
+		void _debug(void);
+		void _info(void);
+		void _warning(void);
+		void _error(void);
+};
+
+#endif;
--- ex05/src/Harl.cpp
+++ ex05/src/Harl.cpp
@@ -6,7 +6,44 @@
 /*   By: jhouyet <jhouyet@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/05 17:12:50 by jhouyet           #+#    #+#             */
-/*   Updated: 2024/06/05 17:12:51 by jhouyet          ###   ########.fr       */
+/*   Updated: 2024/06/05 17:27:45 by jhouyet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Harl.hpp"
+#include <iostream>
+
+Harl::Harl()
+{
+
+}
+
+Harl::~Harl()
+{
+	
+}
+
+void complain(std::string level)
+{
+	
+}
+
+void _debug(void)
+{
+	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do !" << std::endl;
+}
+
+void _info(void)
+{
+	std::cout << "I cannot believe adding extra bacon costs more money. You didn't putenough bacon in my burger ! If you did, I wouldn't be asking for more !" << std::endl;
+}
+
+void _warning(void)
+{
+	std::cout << "I think I deserve to have some extra bacon for free. I've been coming for years whereas you started working here since last month." << std::endl;
+}
+
+void _error(void)
+{
+	std::cout << "This is unacceptable ! I want to speak to the manager now." << std::endl;
+}
