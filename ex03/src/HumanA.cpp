476a8599591f50e0b3b9510f9640182320cbd0c1
Julien Houyet <julien.houyet@gmail.com>
2024-06-05 14:28:47 +0200

    feat: add Weapon

--- ex03/src/HumanA.cpp
+++ ex03/src/HumanA.cpp
@@ -6,7 +6,7 @@
 /*   By: jhouyet <jhouyet@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/05 11:41:50 by jhouyet           #+#    #+#             */
-/*   Updated: 2024/06/05 14:20:08 by jhouyet          ###   ########.fr       */
+/*   Updated: 2024/06/05 14:28:25 by jhouyet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ HumanA::~HumanA()
 
 void HumanA::attack(void) const
 {
-	std::cout << this->_name << " attack with " << this->_weapon.getType() << std::endl;
+	std::cout << this->_name << " attacks with their " << this->_weapon.getType() << std::endl;
 }
 
 void HumanA::setWeapon(const Weapon& weapon)
--- ex03/src/Weapon.cpp
+++ ex03/src/Weapon.cpp
@@ -6,8 +6,27 @@
 /*   By: jhouyet <jhouyet@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/05 11:41:57 by jhouyet           #+#    #+#             */
-/*   Updated: 2024/06/05 11:45:10 by jhouyet          ###   ########.fr       */
+/*   Updated: 2024/06/05 14:27:41 by jhouyet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Weapon.hpp"
+
+Weapon::Weapon(std::string type) : _type(type)
+{
+	
+}
+Weapon::~Weapon()
+{
+	
+}
+
+void Weapon::setType(const std::string type)
+{
+	this->_type = type;
+}
+
+const std::string Weapon::getType() const
+{
+	return this->_type;
+}
