71047056d17549ed0a25a01534492f2da565a86c
Julien Houyet <julien.houyet@gmail.com>
2024-06-05 17:50:02 +0200

    feat: add main

--- ex05/src/Harl.cpp
+++ ex05/src/Harl.cpp
@@ -6,7 +6,7 @@
 /*   By: jhouyet <jhouyet@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/05 17:12:50 by jhouyet           #+#    #+#             */
-/*   Updated: 2024/06/05 17:27:45 by jhouyet          ###   ########.fr       */
+/*   Updated: 2024/06/05 17:47:07 by jhouyet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,27 +23,27 @@ Harl::~Harl()
 	
 }
 
-void complain(std::string level)
+void Harl::complain(std::string level)
 {
-	
+	std::cout << level << std::endl;
 }
 
-void _debug(void)
+void Harl::_debug(void)
 {
 	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do !" << std::endl;
 }
 
-void _info(void)
+void Harl::_info(void)
 {
 	std::cout << "I cannot believe adding extra bacon costs more money. You didn't putenough bacon in my burger ! If you did, I wouldn't be asking for more !" << std::endl;
 }
 
-void _warning(void)
+void Harl::_warning(void)
 {
 	std::cout << "I think I deserve to have some extra bacon for free. I've been coming for years whereas you started working here since last month." << std::endl;
 }
 
-void _error(void)
+void Harl::_error(void)
 {
 	std::cout << "This is unacceptable ! I want to speak to the manager now." << std::endl;
 }
