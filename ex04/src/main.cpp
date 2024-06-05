ca3d5cc628b207011cfaa1246d892774799d1f32
Julien Houyet <julien.houyet@gmail.com>
2024-06-05 15:05:04 +0200

    feat: check args in main

--- ex04/src/main.cpp
+++ ex04/src/main.cpp
@@ -6,14 +6,24 @@
 /*   By: jhouyet <jhouyet@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/05 14:54:36 by jhouyet           #+#    #+#             */
-/*   Updated: 2024/06/05 14:59:00 by jhouyet          ###   ########.fr       */
+/*   Updated: 2024/06/05 15:04:40 by jhouyet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Replace.hpp"
 #include <iostream>
 
-int main()
+int main(int argc, char **argv)
 {
-	std::cout << "Hello Replace" << std::endl;
+	if (argc == 4)
+	{
+		std::cout << "File : " << argv[1] << std::endl;
+		std::cout << "Search : " << argv[2] << std::endl;
+		std::cout << "Replace : " << argv[3] << std::endl;
+	}
+	else
+	{
+		std::cout << "Bad arguments" << std::endl;
+	}
+	return (0);
 }
