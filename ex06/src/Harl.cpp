935eb5c7fb5b34a6c6fc5f4dd1da0a1a736c24cf
Julien Houyet <julien.houyet@gmail.com>
2024-06-05 19:09:26 +0200

    feat: update compain()

--- ex06/src/Harl.cpp
+++ ex06/src/Harl.cpp
@@ -6,7 +6,7 @@
 /*   By: jhouyet <jhouyet@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/05 17:12:50 by jhouyet           #+#    #+#             */
-/*   Updated: 2024/06/05 18:54:38 by jhouyet          ###   ########.fr       */
+/*   Updated: 2024/06/05 19:07:10 by jhouyet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,9 +30,15 @@ void Harl::complain(std::string level)
 {
 	std::string levels[] = { "DEBUG", "INFO", "WARNING", "ERROR" };
 
-	for (int i = 0; i < 4; ++i) {
-		if (levels[i] == level) {
-			(this->*_ftab[i])();
+	for (int i = 0; i < 4; ++i)
+	{
+		if (levels[i] == level)
+		{
+            while (i < 4)
+			{
+                (this->*_ftab[i])();
+				i++;
+            }
 			return ;
 		}
 	}
@@ -41,20 +47,24 @@ void Harl::complain(std::string level)
 
 void Harl::_debug(void)
 {
-	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do !" << std::endl;
+	std::cout << "[ DEBUG ]" << std::endl;
+	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do !\n" << std::endl;
 }
 
 void Harl::_info(void)
 {
-	std::cout << "I cannot believe adding extra bacon costs more money. You didn't putenough bacon in my burger ! If you did, I wouldn't be asking for more !" << std::endl;
+	std::cout << "[ INFO ]" << std::endl;
+	std::cout << "I cannot believe adding extra bacon costs more money. You didn't putenough bacon in my burger ! If you did, I wouldn't be asking for more !\n" << std::endl;
 }
 
 void Harl::_warning(void)
 {
-	std::cout << "I think I deserve to have some extra bacon for free. I've been coming for years whereas you started working here since last month." << std::endl;
+	std::cout << "[ WARNING ]" << std::endl;
+	std::cout << "I think I deserve to have some extra bacon for free. I've been coming for years whereas you started working here since last month.\n" << std::endl;
 }
 
 void Harl::_error(void)
 {
+	std::cout << "[ ERROR ]" << std::endl;
 	std::cout << "This is unacceptable ! I want to speak to the manager now." << std::endl;
 }
