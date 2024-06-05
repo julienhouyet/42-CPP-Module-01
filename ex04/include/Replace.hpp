695c86d0a2a8adf0c3c9189c9d9000370f54ea30
Julien Houyet <julien.houyet@gmail.com>
2024-06-05 15:38:35 +0200

    feat: start Replace class

--- ex04/include/Replace.hpp
+++ ex04/include/Replace.hpp
@@ -6,7 +6,7 @@
 /*   By: jhouyet <jhouyet@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/05 14:54:43 by jhouyet           #+#    #+#             */
-/*   Updated: 2024/06/05 14:58:20 by jhouyet          ###   ########.fr       */
+/*   Updated: 2024/06/05 15:37:58 by jhouyet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,17 @@
 #ifndef __REPLACE_H__
 #define __REPLACE_H__
 
+#include <iostream>
 class Replace
 {
 	public:
-		Replace();
+		Replace(std::string filename, std::string s1, std::string s2);
 		~Replace();
 
 	private:
+		std::string _filename;
+		std::string _s1;
+		std::string _s2;
 };
 
 #endif
--- ex04/src/Replace.cpp
+++ ex04/src/Replace.cpp
@@ -6,8 +6,18 @@
 /*   By: jhouyet <jhouyet@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/05 14:54:39 by jhouyet           #+#    #+#             */
-/*   Updated: 2024/06/05 14:57:54 by jhouyet          ###   ########.fr       */
+/*   Updated: 2024/06/05 15:38:02 by jhouyet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Replace.hpp"
+
+Replace::Replace(std::string filename, std::string s1, std::string s2) : _filename(filename), _s1(s1), _s2(s2)
+{
+	
+}
+
+Replace::~Replace()
+{
+	
+}
