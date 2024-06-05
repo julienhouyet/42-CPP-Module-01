cb51f4ae302dd4849c5df35f129233df0942c5fa
julienhouyet <julien.houyet@gmail.com>
2024-06-05 11:36:03 +0200

    feat: finish ex02

--- ex02/src/main.cpp
+++ ex02/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jhouyet <jhouyet@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/05 11:25:48 by jhouyet           #+#    #+#             */
-/*   Updated: 2024/06/05 11:28:31 by jhouyet          ###   ########.fr       */
+/*   Updated: 2024/06/05 11:35:28 by jhouyet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,22 @@
 
 int	main()
 {
-	std::cout << "Hello" << std::endl;
+	std::string brain;
+	std::string* stringPTR = &brain;
+	std::string& stringREF = brain;
+
+	brain = "HI THIS IS BRAIN";
+	
+	std::cout << "Address :\n" << std::endl;
+	std::cout << "brain : " << &brain << std::endl;
+	std::cout << "stringPTR : " << stringPTR << std::endl;
+	std::cout << "stringREF : " << &stringREF << std::endl;
+
+	std::cout << "" << std::endl;
+	
+	std::cout << "Value :\n" << std::endl;
+	std::cout << "brain : " << brain << std::endl;
+	std::cout << "stringPTR : " << *stringPTR << std::endl;
+	std::cout << "stringREF : " << stringREF << std::endl;
 	return 0;
 }
