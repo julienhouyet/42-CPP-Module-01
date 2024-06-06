44d4b53bdd19e535f74bf577e3261308d0b00bee
Julien Houyet <julien.houyet@gmail.com>
2024-06-06 14:49:05 +0200

    fix: add nullptr on weapon HumanB by default

--- ex03/src/HumanB.cpp
+++ ex03/src/HumanB.cpp
@@ -6,13 +6,13 @@
 /*   By: jhouyet <jhouyet@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/05 11:41:53 by jhouyet           #+#    #+#             */
-/*   Updated: 2024/06/05 14:44:53 by jhouyet          ###   ########.fr       */
+/*   Updated: 2024/06/06 14:48:23 by jhouyet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "HumanB.hpp"
 
-HumanB::HumanB(std::string name) : _name(name)
+HumanB::HumanB(std::string name) : _name(name), _weapon(nullptr)
 {
 }
 
